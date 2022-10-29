b387845aff2e154ad9f13e710b54828a9710d4c0
Elias Emon <019eliashossain@gmail.com>
2022-10-29 19:29:53 +0600

    graphql added

---
+++ config/env/production/plugins.js
@@ -0,0 +1,12 @@
+module.exports = {
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 10,
+      amountLimit: 100,
+      apolloServer: { tracing: false, introspection: true },
+    },
+  },
+};
