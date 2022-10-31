e625bc5b8ee280bd732a2dc201209db3bd9ff1e9
Elias Emon <019eliashossain@gmail.com>
2022-10-31 16:03:47 +0600

    EveryThing is working perfectly now

--- src/api/catagory/controllers/catagory.js
+++ src/api/catagory/controllers/catagory.js
@@ -6,29 +6,31 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::catagory.catagory',({strapi})=>({
-    async find(ctx){
-        try {
-            ctx.query = { ...ctx.query, local: 'en' , populate : "*"};
-            console.log(ctx.query)
-            const { data , meta } = await super.find(ctx);
-            const newData = data.map((data)=>{
-                return {
-                    "id" : data.id,
-                    "name" : data.attributes.name,
-                    "catagoryId":  data?.attributes?.catagoryId,
-                    "sub_catagories" : data.attributes?.sub_catagories?.data.map((data)=>{
-                        return {
-                            "id" : data?.id,
-                            "name" : data?.attributes?.name,
-                            "subCatagoryID":  data?.attributes?.subCatagoryID,
-                        }
-                    })
-                }
-            })
-            return { data : newData , meta}      
-        } catch (error) {
-            ctx.body = error
-        }
-    }
-}));
+module.exports = createCoreController('api::catagory.catagory');
+
+// ,({strapi})=>({
+//     async find(ctx){
+//         try {
+//             ctx.query = { ...ctx.query, local: 'en' , populate : "*"};
+//             console.log(ctx.query)
+//             const { data , meta } = await super.find(ctx);
+//             const newData = data.map((data)=>{
+//                 return {
+//                     "id" : data.id,
+//                     "name" : data.attributes.name,
+//                     "catagoryId":  data?.attributes?.catagoryId,
+//                     "sub_catagories" : data.attributes?.sub_catagories?.data.map((data)=>{
+//                         return {
+//                             "id" : data?.id,
+//                             "name" : data?.attributes?.name,
+//                             "subCatagoryID":  data?.attributes?.subCatagoryID,
+//                         }
+//                     })
+//                 }
+//             })
+//             return { data : newData , meta}      
+//         } catch (error) {
+//             ctx.body = error
+//         }
+//     }
+// })
--- src/api/sub-catagory/controllers/sub-catagory.js
+++ src/api/sub-catagory/controllers/sub-catagory.js
@@ -6,27 +6,28 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::sub-catagory.sub-catagory',({strapi})=>({
-    async find(ctx){
-        try {
-            ctx.query = { ...ctx.query, local: 'en' };
-            const { data, meta } = await super.find(ctx);
-            const newData = data.map((data)=>{
-                return {
-                    "id" : data.id,
-                    "name" : data.attributes.name,
-                    "subCatagoryID":  data?.attributes?.subCatagoryID,
-                    "catagories" : data?.attributes?.catagories?.data.map((data)=>{
-                        return {
-                            "id" : data?.id,
-                            "name" : data?.attributes?.name,
-                        }
-                    })
-                }
-            })
-            return {newData , meta}      
-        } catch (error) {
-            ctx.body = error
-        }
-    }
-}));
+module.exports = createCoreController('api::sub-catagory.sub-catagory');
+// ,({strapi})=>({
+//     async find(ctx){
+//         try {
+//             ctx.query = { ...ctx.query, local: 'en' };
+//             const { data, meta } = await super.find(ctx);
+//             const newData = data.map((data)=>{
+//                 return {
+//                     "id" : data.id,
+//                     "name" : data.attributes.name,
+//                     "subCatagoryID":  data?.attributes?.subCatagoryID,
+//                     "catagories" : data?.attributes?.catagories?.data.map((data)=>{
+//                         return {
+//                             "id" : data?.id,
+//                             "name" : data?.attributes?.name,
+//                         }
+//                     })
+//                 }
+//             })
+//             return {newData , meta}      
+//         } catch (error) {
+//             ctx.body = error
+//         }
+//     }
+// })
