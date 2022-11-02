430bd6b71874159f6c79c277ea36fae2593d3476
Elias Emon <019eliashossain@gmail.com>
2022-11-02 18:01:35 +0600

    Cors added

--- config/middlewares.js
+++ config/middlewares.js
@@ -14,8 +14,15 @@ module.exports = [
       },
     },
   },
-  'strapi::cors',
   'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['*']
+    }
+  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
